e07145b4818868ef5749ef91f3f7a764b9aa6d4c
Maria Castro <mariacastro87@gmail.com>
2017-11-29 23:02:17 -0300

    Set Types in app.ts methods

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -8,28 +8,36 @@ import 'phaser'
 const logoImage = require('assets/phaser.png')
 
 class SimpleGame {
-
+    game: Phaser.Game;    
     constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create })
+        const config: Phaser.IGameConfig = {
+            width:           800, 
+            height:          600,
+            renderer:        Phaser.AUTO,   
+            parent:          'content',
+            resolution:      1,
+            forceSetTimeOut: false,
+            state: { preload: this.preload, create: this.create }
+        }
+        this.game = new Phaser.Game(config)
     }
 
-    game: Phaser.Game;
 
     preload() {
         this.game.load.image('logo', logoImage)
     }
 
     create() {
-        const logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo')
+        const logo: Phaser.Sprite = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo')
 
-        const textAlign = {
+        const textStyle: Phaser.PhaserTextStyle = {
             font: "15px Arial", 
             fill: "#19de65" ,
             align:  'left',
             boundsAlignH: 'center',
             boundsAlignV: 'bottom'
         }
-        const text = this.game.add.text(0, 0, 'Hello World!', textAlign)
+        const text: Phaser.Text = this.game.add.text(0, 0, 'Hello World!', textStyle)
         
         text.setTextBounds(16, 16, 768, 568)
         logo.anchor.setTo(0.5, 0.5)
